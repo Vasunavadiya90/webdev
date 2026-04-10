af5910d37aab936f1cb9b9d3aa8e059aa1983ed4
vasu <vasunavadiya923@gmail.com>
2026-04-10 17:42:51 +0530

    save changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/webdev/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/digital-flagship" element={<DigitalFlagship />} />
